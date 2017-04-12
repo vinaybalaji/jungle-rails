d8b332ad079078516bc00453d48408287ad4ff4c
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-12 06:07:46 +0000

    Added partial flash messages. Validation controls need to be worked on

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      flash[:notice] = "Account created successfully!"
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
       redirect_to '/signup'
     end
   end
