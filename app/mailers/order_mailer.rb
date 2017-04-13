41b5d4a03e0a69f3fc92e5823ccfb22bdf46561d
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-13 18:01:50 +0000

    added confirmation email features

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,13 +2,7 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation_email(order)
       @order = order
-      # puts "?????????????"
-      # puts @order
-      # puts "?????????????"
       @line_items = LineItem.where(order_id: @order.id)
-      puts "<<<<<<<<<<<>>>>>>>>>>>>"
-      puts @order.email
-      puts "<<<<<<<<<<<>>>>>>>>>>>>"
       mail(to: @order.email, subject: 'Your Jungle Order#' + @order.id.to_s + 'has been placed.')
   end
 end
