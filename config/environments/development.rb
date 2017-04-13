219a79ecd0336864636ab447b9cb9380851883ac
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-13 19:03:14 +0000

    Added feature of HTTP auth to access admin pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "vinaylighthouselabs@gmail.com",
-    :password             => "measuretwicecutonce",
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
