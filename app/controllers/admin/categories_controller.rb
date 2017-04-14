78d78439cceb4e0a1f178a32d2da8427e45ac3e6
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-14 05:31:52 +0000

    Fixes for passing reviews to category browsing pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,7 +13,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories]
     else
       render :new
     end
