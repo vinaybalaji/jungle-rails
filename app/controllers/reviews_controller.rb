dbee4ebb26048f57795208a47e8df4762f91a2bd
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-14 01:04:29 +0000

    Got the feature to add reviews to work.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,23 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    review = create_review
+    if review.valid?
+      redirect_to :back
+    else 
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def create_review
+    review = Review.new(
+      product_id: params[:product_id].to_i,
+      description: params[:review][:description],
+      rating: params[:review][:rating].to_i,
+      user_id: current_user.id
+    )
+    review.save!
+    review
   end
 end
