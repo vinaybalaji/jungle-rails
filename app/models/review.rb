148caf1cfc29242a8c5f505b9bcc8f43f00f219a
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-14 00:35:48 +0000

    Progressed on feature up until setting the routes and controlers. Have to define the review save action next

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,13 @@
 class Review < ActiveRecord::Base
+  attr_accessor :rating_options
+
   belongs_to :product
   belongs_to :user
 
   validates :rating, presence: true
+
+  def initiatize
+    @rating_options = [1, 2, 3, 4, 5]
+  end
 
 end
