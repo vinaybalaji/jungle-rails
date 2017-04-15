299b27a94ff7360612c7dd742cc3dc4b23224d60
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-15 23:33:10 +0000

    Removed extra spacing in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,21 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
-
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:create, :destroy]
   end
-  
   resources :categories, only: [:show]
-
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
   end
-
   resources :orders, only: [:create, :show]
-  
   resources :users, only: [:create, :new]
-
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
-
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
-
 end
