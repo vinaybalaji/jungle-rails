8de2cd8cc6143a8205b2dc30f39603c2b9de8292
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-16 01:00:34 +0000

    Cleaned up users controller and sessions controller for DRY code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  
   def new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    new_user = create_user
+    if new_user && new_user.valid?
+      session[:user_id] = new_user.id
       redirect_to root_path
     else
       redirect_to :back
@@ -14,28 +15,40 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    if params[:user][:first_name].blank? || params[:user][:last_name].blank?
-      flash[:notice] = "Oops, couldn't create account. Please make sure you enter a first name and last name."
+  def create_user
+    user = params[:user]
+    first_name = user[:first_name]
+    last_name = user[:last_name]
+    email = user[:email]
+    password = user[:password]
+    password_confirmation = user[:password_confirmation]
+    if first_name.blank? || last_name.blank?
+      message = "Oops! Please make sure you enter a first name and last name."
+    end
+    if email.blank?
+      message = "Oops! Please make sure you are entering a valid email and try again."
+    end
+    if password.blank? || password_confirmation.blank?
+      message = "Oops! Please make sure you are entering a valid password."
+    end
+    if password != password_confirmation
+      message = "Oops! Passwords don't match. Please try again."
+    end
+    if User.exists?(email: email)
+      message = "Oops! Please try again or log in with your existing email."
+    end
+    if message
+      flash[:notice] = message
       return
     end
-    if params[:user][:email].blank?
-      flash[:notice] = "Oops, couldn't create account. Please make sure you are entering a valid email and try again."
-      return
-    end
-    if params[:user][:password].blank? || params[:user][:password_confirmation].blank?
-      flash[:notice] = "Oops, couldn't create account. Please make sure you are entering a valid password."
-      return
-    end
-    if params[:user][:password] != params[:user][:password_confirmation]
-      flash[:notice] = "Oops, passwords don't match. Please try again."
-      return
-    end
-    if User.exists?(email: params[:user][:email])
-      flash[:notice] = "Oops, account already exists. Please try again or log in with your existing email."
-      return
-    end
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    new_user = User.new(
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: password
+    )
+    new_user.save!
+    new_user
   end
-
+  
 end
