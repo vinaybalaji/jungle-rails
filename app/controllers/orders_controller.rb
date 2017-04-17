13a815cd864a5f8d7eb5479d296835febf8aa7a7
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-17 03:05:06 +0000

    Final DRY Code Changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @line_items = @order.line_items
   end
 
   def create
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,7 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation_email(order)
       @order = order
-      @line_items = LineItem.where(order_id: @order.id)
       mail(to: @order.email, subject: 'Your Jungle Order#' + @order.id.to_s + 'has been placed.')
   end
+  
 end
